870fea6db22b3399e9b5942cdbf0bf046c9a6f0b
19102486 <19102486@usc.edu.ph>
2023-12-26 17:47:43 +0800

    added google-sign-in UI

--- lib/data/auth_data.dart
+++ lib/data/auth_data.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobdev_final_app/data/firestore.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthenticationDatasource {
   Future<void> register(String email, String password, String PasswordConfirm);
   Future<void> login(String email, String password);
+  // Future<UserCredential?> signInWithGoogle();
 }
 
 class AuthenticationRemote extends AuthenticationDatasource {
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
+  // final GoogleSignIn _googleSignIn = GoogleSignIn();
+
   @override
   Future<void> login(String email, String password) async {
     await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -25,4 +30,10 @@ class AuthenticationRemote extends AuthenticationDatasource {
       });
     }
   }
+
+
+  // @override
+  // Future<UserCredential?> signInWithGoogle() {
+  //   // complete the code for the signInWithGoogle 
+  // }
 }
